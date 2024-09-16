e64fea2dbe82934f7214592c03b565b6d9eb0e2d
wilson-2009 <DOGE1314520@gmail.com>
2024-09-16 09:20:50 -0400

    doing odd or even challenge

---
+++ codes.py
@@ -0,0 +1,31 @@
+""" x = 3
+y = float(3)
+print(x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(values[5]) """
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+""" day_of_week = input("what day is it?")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+""" x = "test"
+print(f"hello {x}") """
+
+""" temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
---
+++ odd or even.py
@@ -0,0 +1,8 @@
+number = input("give me a number")
+
+if number > 68:
+    print('warm')
+elif number == 68:
+    print('perfect')
+else:
+    print('cold')
