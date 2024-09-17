20a261429028aacccc5edc88ecdd911a0a83f191
wilson-2009 <DOGE1314520@gmail.com>
2024-09-17 09:20:52 -0400

    doing how many words in sentence challenge

--- odd or even.py
+++ odd or even.py
@@ -1,8 +1,13 @@
-number = input("give me a number")
-
+""" number = 75
 if number > 68:
     print('warm')
 elif number == 68:
     print('perfect')
 else:
-    print('cold')
+    print('cold') """
+
+sentence = input("give me a sentence")
+if sentence == "Friday":
+    print(f"{x} words")
+else:
+    print(f"{x} words")
