1f75904aae466cfbe2d10e9c812b4653c9b23fad
wilson-2009 <DOGE1314520@gmail.com>
2024-09-19 09:20:46 -0400

    doing number factor

--- bill.py
+++ bill.py
@@ -1,10 +1,10 @@
-bill = input("what was the bill")
+bill = int(input("what was the bill"))
 tip = input("was the service bad, okay, good, or great")
 if tip == ("bad"):
-    print (f"your bill is {bill * 1}")
+    print (f"your bill is {bill*1}")
 elif tip == ("okay"):
-    print (f"your bill is {bill * 1.15}")
+    print (f"your bill is {bill*1.15}")
 elif tip == ("good"):
-    print (f"your bill is {bill * 1.2}")
+    print (f"your bill is {bill*1.2}")
 elif tip == ("great"):
-    print (f"your bill is {bill * 1.25}")
+     print (f"your bill is {bill*1.25}")
---
+++ words in sentence.py
@@ -0,0 +1,5 @@
+x = "this is a thing"
+y= x.split( )
+z = y[3]
+print(y)
+print(z)
