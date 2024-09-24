bac8ff50ef273a40845376952beedcff78174f55
wilson-2009 <DOGE1314520@gmail.com>
2024-09-24 09:20:45 -0400

    greatest common factor

--- greatest common factor.py
+++ greatest common factor.py
@@ -1,1 +1,15 @@
 num1 = int(input("give me your first number"))
+num2 = int(input("give me your second number"))
+
+print(num1)
+print(num2)
+
+for i in range(1, num1 + 1):
+    if num1 % i == 0:
+        print(i)
+
+for i in range(1, num2 + 1):
+    if num2 % i == 0:
+        print(i)
+
+if num1 % num2 == 0
