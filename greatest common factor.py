2e9eeae26b266875e351185e9adeba5b94141b23
wilson-2009 <DOGE1314520@gmail.com>
2024-09-25 09:20:27 -0400

    doing gcf

--- greatest common factor.py
+++ greatest common factor.py
@@ -1,15 +1,13 @@
 num1 = int(input("give me your first number"))
 num2 = int(input("give me your second number"))
+def gcf(x, y):
+    if x > y:
+        num = y
+    else:
+        num = x
+    for i in range(1, num+1):
+        if((x % i == 0) and (y % i == 0)):
+            word = i
+    return word
 
-print(num1)
-print(num2)
-
-for i in range(1, num1 + 1):
-    if num1 % i == 0:
-        print(i)
-
-for i in range(1, num2 + 1):
-    if num2 % i == 0:
-        print(i)
-
-if num1 % num2 == 0
+print("gcf is", gcf(x,y))
